63e111ff21ccc6724bb916b6a21a4af24bed7ff4
vzambon <vszambon@gmail.com>
2025-08-26 18:36:07 -0300

    fix: update environment variable prefixes in setup-entrypoint.sh

--- entrypoints/setup-entrypoint.sh
+++ entrypoints/setup-entrypoint.sh
@@ -25,57 +25,57 @@ file_env() {
 }
 
 # Variáveis padrão
-DB_TYPE=${DB_TYPE:-pgsql}
-DB_HOST=${DB_HOST:-pgsql}
-DB_PORT=${DB_PORT:-5432}
-DB_NAME=${DB_NAME:-limesurvey}
-DB_TABLE_PREFIX=${DB_TABLE_PREFIX:-lime_}
-DB_USERNAME=${DB_USERNAME:-limesurvey}
-file_env DB_PASSWORD
+LS_DB_TYPE=${LS_DB_TYPE:-pgsql}
+LS_DB_HOST=${LS_DB_HOST:-pgsql}
+LS_DB_PORT=${LS_DB_PORT:-5432}
+LS_DB_NAME=${LS_DB_NAME:-limesurvey}
+LS_DB_TABLE_PREFIX=${LS_DB_TABLE_PREFIX:-lime_}
+LS_DB_USERNAME=${LS_DB_USERNAME:-limesurvey}
+file_env LS_DB_PASSWORD
 
-ADMIN_USER=${ADMIN_USER:-admin}
-ADMIN_NAME=${ADMIN_NAME:-Admin}
-ADMIN_EMAIL=${ADMIN_EMAIL:-admin@admin.com}
-file_env ADMIN_PASSWORD
+LS_ADMIN_USER=${LS_ADMIN_USER:-admin}
+LS_ADMIN_NAME=${LS_ADMIN_NAME:-Admin}
+LS_ADMIN_EMAIL=${LS_ADMIN_EMAIL:-admin@admin.com}
+file_env LS_ADMIN_PASSWORD
 
-DB_CHARSET=${DB_CHARSET:-utf8mb4}
-DB_MYSQL_ENGINE=${DB_MYSQL_ENGINE:-InnoDB}
-DEFAULT_LANGUAGE=${DEFAULT_LANGUAGE:-pt-BR}
-DEBUG=${DEBUG:-0}
-DEBUGSQL=${DEBUGSQL:-0}
-SESSION_NAME=${SESSION_NAME:-LS-LRRCYVEOLCUJVOLI}
-URL_FORMAT=${URL_FORMAT:-path}
-SHOW_SCRIPT_NAME=${SHOW_SCRIPT_NAME:-true}
-BASE_URL=${BASE_URL:-}
-PUBLIC_URL=${PUBLIC_URL:-http://localhost/}
+LS_DB_CHARSET=${LS_DB_CHARSET:-utf8mb4}
+LS_DB_MYSQL_ENGINE=${LS_DB_MYSQL_ENGINE:-InnoDB}
+LS_DEFAULT_LANGUAGE=${LS_DEFAULT_LANGUAGE:-pt-BR}
+LS_DEBUG=${LS_DEBUG:-0}
+LS_DEBUGSQL=${LS_DEBUGSQL:-0}
+LS_SESSION_NAME=${LS_SESSION_NAME:-LS-LRRCYVEOLCUJVOLI}
+LS_URL_FORMAT=${LS_URL_FORMAT:-path}
+LS_SHOW_SCRIPT_NAME=${LS_SHOW_SCRIPT_NAME:-true}
+LS_BASE_URL=${LS_BASE_URL:-'/limesurvey'}
+LS_SESSION_DOMAIN=${LS_SESSION_DOMAIN:-.localhost}
 
 # Gera config.php se não existir
 if [ ! -f application/config/config.php ]; then
     echo "Info: Generating config.php"
 
-    sed "s|{{DB_TYPE}}|$DB_TYPE|g; \
-         s|{{DB_HOST}}|$DB_HOST|g; \
-         s|{{DB_PORT}}|$DB_PORT|g; \
-         s|{{DB_NAME}}|$DB_NAME|g; \
-         s|{{DB_USER}}|$DB_USERNAME|g; \
-         s|{{DB_PASSWORD}}|$DB_PASSWORD|g; \
-         s|{{DB_TABLE_PREFIX}}|$DB_TABLE_PREFIX|g; \
-         s|{{DB_CHARSET}}|$DB_CHARSET|g
-         s|{{DB_MYSQL_ENGINE}}|$DB_MYSQL_ENGINE|g; \
-         s|{{URL_FORMAT}}|$URL_FORMAT|g; \
-         s|{{SHOW_SCRIPT_NAME}}|$SHOW_SCRIPT_NAME|g; \
-         s|{{BASE_URL}}|$BASE_URL|g; \
-         s|{{DEBUG}}|$DEBUG|g; \
-         s|{{DEBUGSQL}}|$DEBUGSQL|g; \
-         s|{{PUBLIC_URL}}|$PUBLIC_URL|g; \
-         s|{{SESSION_NAME}}|$SESSION_NAME|g; \
-         s|{{DEFAULT_LANGUAGE}}|$DEFAULT_LANGUAGE|g;" \
+    sed "s|{{DB_TYPE}}|$LS_DB_TYPE|g; \
+         s|{{DB_HOST}}|$LS_DB_HOST|g; \
+         s|{{DB_PORT}}|$LS_DB_PORT|g; \
+         s|{{DB_NAME}}|$LS_DB_NAME|g; \
+         s|{{DB_USER}}|$LS_DB_USERNAME|g; \
+         s|{{DB_PASSWORD}}|$LS_DB_PASSWORD|g; \
+         s|{{DB_TABLE_PREFIX}}|$LS_DB_TABLE_PREFIX|g; \
+         s|{{DB_CHARSET}}|$LS_DB_CHARSET|g
+         s|{{DB_MYSQL_ENGINE}}|$LS_DB_MYSQL_ENGINE|g; \
+         s|{{URL_FORMAT}}|$LS_URL_FORMAT|g; \
+         s|{{SHOW_SCRIPT_NAME}}|$LS_SHOW_SCRIPT_NAME|g; \
+         s|{{BASE_URL}}|$LS_BASE_URL|g; \
+         s|{{DEBUG}}|$LS_DEBUG|g; \
+         s|{{DEBUGSQL}}|$LS_DEBUGSQL|g; \
+         s|{{PUBLIC_URL}}|$LS_PUBLIC_URL|g; \
+         s|{{SESSION_NAME}}|$LS_SESSION_NAME|g; \
+         s|{{DEFAULT_LANGUAGE}}|$LS_DEFAULT_LANGUAGE|g;" \
          application/config/config.php.template > application/config/config.php
 fi
 
 # Check if database is available
-if [ -z "$DB_SOCK" ]; then
-    until nc -z -v -w30 "$DB_HOST" "$DB_PORT"
+if [ -z "$LS_DB_SOCK" ]; then
+    until nc -z -v -w30 "$LS_DB_HOST" "$LS_DB_PORT"
     do
         echo "Info: Waiting for database connection..."
         sleep 5
@@ -88,20 +88,9 @@ fi
 php application/commands/console.php updatedb
 
 # Cria admin se necessário
-php application/commands/console.php user:exists "$ADMIN_USER" >/dev/null 2>&1
+php application/commands/console.php user:exists "$LS_ADMIN_USER" >/dev/null 2>&1
 if [ $? -ne 0 ]; then
-    php application/commands/console.php install "$ADMIN_USER" "$ADMIN_PASSWORD" "$ADMIN_NAME" "$ADMIN_EMAIL"
-fi
-
-# Install default plugins
-if [ -d protiviti-defaults/plugins ]; then
-    for plugin in protiviti-defaults/plugins/*; do
-        if [ -d "$plugin" ]; then
-            echo "Info: Installing plugin $(basename "$plugin")"
-            echo "Running installuploadedplugin for $(basename "$plugin")"
-            php application/commands/console.php installuploadedplugin --pluginName="$(basename "$plugin")"
-        fi
-    done
+    php application/commands/console.php install "$LS_ADMIN_USER" "$LS_ADMIN_PASSWORD" "$LS_ADMIN_NAME" "$LS_ADMIN_EMAIL"
 fi
 
 # Install uploaded plugins
